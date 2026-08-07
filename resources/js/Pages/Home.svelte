c9483be29b82c991404133253d8f1261e6932582
Ian Ewing <ian@ewing.codes>
2026-08-07 16:06:14 -0400

    linting

--- resources/js/Pages/Home.svelte
+++ resources/js/Pages/Home.svelte
@@ -5,4 +5,5 @@
 <main class="p-6">
   <h1 class="text-2xl font-bold">{title}</h1>
   <p>Welcome to your NativePHP + Inertia + Svelte app.</p>
+    <button>Get Games</button>
 </main>
--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,12 @@
 
 use App\NativeComponents\Home;
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::native('/', Home::class);
 
 Route::get('/svelte', function () {
-    return \Inertia\Inertia::render('Home', [
+    return Inertia::render('Home', [
         'title' => 'Steam Library Tracker',
     ]);
 });
